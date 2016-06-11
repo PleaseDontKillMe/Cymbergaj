a12fd4e3fa94c01c9982642ce7eb5ab36b49b40f
Daniel <wilkowski.kontakt@gmail.com>
2016-06-11 18:42:09 +0100

    Used AnimatedSpriteSheet for SpaceshipRenderer

--- src/Application/View/SpaceshipRenderer.java
+++ src/Application/View/SpaceshipRenderer.java
@@ -1,7 +1,6 @@
 package Application.View;
 
 
-import Application.Geometry.Point;
 import Application.Model.World.Spaceship;
 
 import java.awt.*;
@@ -10,19 +9,16 @@ import java.awt.*;
 public class SpaceshipRenderer implements Renderer {
 
     private final Spaceship spaceship;
-    private ImagesRepository images;
+    private AnimatedSpriteSheet spriteSheet;
 
     public SpaceshipRenderer(Spaceship spaceship, ImagesRepository images) {
         this.spaceship = spaceship;
-        this.images = images;
+        spriteSheet = images.spaceship;
     }
 
     @Override
     public void renderOn(Graphics2D canvas) {
-
-        Point position = spaceship.getPosition();
-
-        canvas.drawImage(images.plane, position.getX(), position.getY(), 206, 151, null);
-
+        spriteSheet.drawOn(canvas, spaceship.getPosition());
+        spriteSheet = spriteSheet.next();
     }
 }
