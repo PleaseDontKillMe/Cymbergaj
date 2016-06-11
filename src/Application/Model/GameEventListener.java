f20d3336e272e495e729729ce348e9ce9680fa17
Daniel <wilkowski.kontakt@gmail.com>
2016-06-11 17:16:16 +0100

    Clean

--- src/Application/Model/GameEventListener.java
+++ src/Application/Model/GameEventListener.java
@@ -2,10 +2,15 @@ package Application.Model;
 
 
 public interface GameEventListener {
+    default void render() {
+    }
 
-    default void render() { }
-    default void update(){ }
-    default void fps(double fps) { }
-    default void gameEnd() { }
+    default void update() {
+    }
 
+    default void fps(double fps) {
+    }
+
+    default void gameEnd() {
+    }
 }
