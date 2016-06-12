3964b630a5e85979c4e6701097cd6e09a0c0f6ce
Daniel <wilkowski.kontakt@gmail.com>
2016-06-12 14:48:39 +0200

    Clean

--- src/Application/View/ExplosionRenderer.java
+++ src/Application/View/ExplosionRenderer.java
@@ -9,13 +9,13 @@ import java.awt.Graphics2D;
 public class ExplosionRenderer implements Renderer {
     private final Explosion explosion;
     private AnimatedSpriteSheet spriteSheet;
+    private int refreshesToNextFrame = 0;
 
     public ExplosionRenderer(Explosion explosion, ImagesRepository images) {
         this.explosion = explosion;
         this.spriteSheet = images.explosion;
     }
 
-    int refreshesToNextFrame = 0;
     @Override
     public void renderOn(Graphics2D canvas) {
         spriteSheet.drawOn(canvas, explosion.getPosition());
