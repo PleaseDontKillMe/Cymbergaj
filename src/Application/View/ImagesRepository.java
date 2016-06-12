da09655829f4c42f388e6a028f46b70d90c398c3
Daniel Wilkowski <h4t3r2.g0nn4.ha73@gmail.com>
2016-06-12 13:59:30 +0200

    Used RowedAnimatedSpriteSheet to load explosion.png

--- src/Application/View/ImagesRepository.java
+++ src/Application/View/ImagesRepository.java
@@ -15,6 +15,7 @@ public class ImagesRepository {
     BufferedImage background2;
     BufferedImage fireball;
     AnimatedSpriteSheet spaceship;
+    AnimatedSpriteSheet explosion;
 
     public void load() {
         plane = loadImage("plane.png");
@@ -22,6 +23,7 @@ public class ImagesRepository {
         background2 = loadImage("space-background2.png");
         fireball = loadImage("fireball.png");
         spaceship = loadAnimatedImage("spaceship.png", new Size(64, 64), 32);
+        explosion = loadAnimatedImage("explosion.png", new Size(64, 64), 25, 5);
     }
 
     private BufferedImage loadImage(String filename) {
@@ -35,5 +37,10 @@ public class ImagesRepository {
     private AnimatedSpriteSheet loadAnimatedImage(String filename, Size spriteSize, int spritesAmount) {
         BufferedImage image = loadImage(filename);
         return new AnimatedSpriteSheet(image, spriteSize, spritesAmount);
+    }
+
+    private AnimatedSpriteSheet loadAnimatedImage(String filename, Size spriteSize, int spritesAmount, int columnsCount) {
+        BufferedImage image = loadImage(filename);
+        return new RowedAnimatedSpriteSheet(image, spriteSize, spritesAmount, columnsCount);
     }
 }
