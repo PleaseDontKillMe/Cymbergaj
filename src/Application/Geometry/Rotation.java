e54195406e962648be29a376e0f80b332aec1557
Daniel <wilkowski.kontakt@gmail.com>
2016-06-12 23:32:52 +0200

    Created Rotation

---
+++ src/Application/Geometry/Rotation.java
@@ -0,0 +1,19 @@
+package Application.Geometry;
+
+public class Rotation {
+    private final Angle angle;
+    private final Point center;
+
+    public Rotation(Angle angle, Point center) {
+        this.angle = angle;
+        this.center = center;
+    }
+
+    public Angle getAngle() {
+        return angle;
+    }
+
+    public Point getCenter() {
+        return center;
+    }
+}
