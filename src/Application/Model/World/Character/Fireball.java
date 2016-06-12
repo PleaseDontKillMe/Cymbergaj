23214da9d0817e45e98843559c69ae2c3ae9282e
Daniel <wilkowski.kontakt@gmail.com>
2016-06-12 21:33:15 +0200

    Added angle to the fireball

--- src/Application/Model/World/Character/Fireball.java
+++ src/Application/Model/World/Character/Fireball.java
@@ -1,22 +1,29 @@
 package Application.Model.World.Character;
 
+import Application.Geometry.Angle;
 import Application.Geometry.Point;
 import Application.View.Renderer.FireballRenderer;
 import Application.View.Renderer.ImagesRepository;
 import Application.View.Renderer.Renderer;
 
 public class Fireball {
-    private Point position;
+    private final Point position;
+    private final Angle direction;
 
-    public Fireball(Point position) {
+    public Fireball(Point position, Angle direction) {
         this.position = position;
-    }
-
-    public Renderer getRenderer(ImagesRepository images) {
-        return new FireballRenderer(this, images);
+        this.direction = direction;
     }
 
     public Point getPosition() {
         return position;
+    }
+
+    public Angle getAngle() {
+        return direction;
+    }
+
+    public Renderer getRenderer(ImagesRepository images) {
+        return new FireballRenderer(this, images);
     }
 }
