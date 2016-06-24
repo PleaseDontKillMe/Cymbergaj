c20bc0cf33d11bb23aa34fcf51224c4da0b04a86
Daniel <wilkowski.kontakt@gmail.com>
2016-06-25 00:54:14 +0100

    Used Fireball instead of Ball

--- src/main/java/danon/Cymbergaj/Model/World/Character/Fireball.java
+++ src/main/java/danon/Cymbergaj/Model/World/Character/Fireball.java
@@ -1,37 +1,19 @@
 package danon.Cymbergaj.Model.World.Character;
 
-import danon.Cymbergaj.Geometry.Angle;
-import danon.Cymbergaj.Geometry.Point;
-import danon.Cymbergaj.Model.World.Control.FireballControl;
+import danon.Cymbergaj.Debug.dyn.GameObject;
 import danon.Cymbergaj.View.Renderer.FireballRenderer;
 import danon.Cymbergaj.View.Renderer.ImagesRepository;
 import danon.Cymbergaj.View.Renderer.Renderable;
 
-public class Fireball extends Body {
-    private final Angle direction;
+import java.awt.*;
+
+public class Fireball extends GameObject {
 
     public static float VELOCITY = 0.5f;
 
-    public Fireball(Point position, Angle direction) {
-        super(position);
-        this.direction = direction;
-    }
-
-    public Angle getDirection() {
-        return direction;
-    }
-
-    public float getVelocity() {
-        return VELOCITY;
-    }
-
-    public FireballControl getControl() {
-        return new FireballControl(this);
-    }
-
     @Override
-    public int getRadius() {
-        return 8;
+    public void renderMe(Graphics2D canvas) {
+
     }
 
     public Renderable getRenderer(ImagesRepository images) {
