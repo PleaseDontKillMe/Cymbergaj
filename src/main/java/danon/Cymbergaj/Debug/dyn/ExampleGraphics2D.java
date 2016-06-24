dacc1c80cd1decabc85fe5354fef72b30413c6ca
Daniel <wilkowski.kontakt@gmail.com>
2016-06-24 22:40:15 +0100

    Recreated body states

--- src/main/java/danon/Cymbergaj/Debug/dyn/ExampleGraphics2D.java
+++ src/main/java/danon/Cymbergaj/Debug/dyn/ExampleGraphics2D.java
@@ -13,9 +13,6 @@ import java.awt.event.WindowEvent;
 import java.awt.geom.AffineTransform;
 import java.awt.image.BufferStrategy;
 
-import static javax.swing.JFrame.*;
-import static javax.swing.JFrame.EXIT_ON_CLOSE;
-
 
 public class ExampleGraphics2D extends JFrame {
 
@@ -26,6 +23,7 @@ public class ExampleGraphics2D extends JFrame {
     protected World world;
     protected boolean stopped = false;
     protected long last;
+    private Dimension size;
 
     public ExampleGraphics2D() {
         super("Graphics2D Example");
@@ -39,7 +37,7 @@ public class ExampleGraphics2D extends JFrame {
             }
         });
 
-        Dimension size = new Dimension(800, 600);
+        size = new Dimension(1000, 800);
 
         canvas.setPreferredSize(size);
         canvas.setMinimumSize(size);
@@ -55,104 +53,56 @@ public class ExampleGraphics2D extends JFrame {
 
     protected void initializeWorld() {
         this.world = new World();
+        world.setGravity(new Vector2(0,0));
 
         // create the floor
-        Rectangle floorRect = new Rectangle(15.0, 1.0);
-        GameObject floor = new GameObject();
-        floor.addFixture(new BodyFixture(floorRect));
-        floor.setMass(MassType.INFINITE);
-        // move the floor down a bit
-        floor.translate(0.0, -4.0);
-        this.world.addBody(floor);
+        GameObject floor1 = new GameObject(), floor2 = new GameObject();
 
-        // create a triangle object
-        Triangle triShape = new Triangle(new Vector2(0.0, 0.5), new Vector2(-0.5, -0.5), new Vector2(0.5, -0.5));
-        GameObject triangle = new GameObject();
-        triangle.addFixture(triShape);
-        triangle.setMass(MassType.NORMAL);
-        triangle.translate(-1.0, 2.0);
-        triangle.getLinearVelocity().set(5.0, 0.0);
-        this.world.addBody(triangle);
+        Rectangle horizontalRectangle = new Rectangle(20.0, 0.2);
+        floor1.addFixture(new BodyFixture(horizontalRectangle));
+        floor1.setMass(MassType.INFINITE);
+        floor2.addFixture(new BodyFixture(horizontalRectangle));
+        floor2.setMass(MassType.INFINITE);
 
-        // create a circle
-        Circle cirShape = new Circle(0.5);
-        GameObject circle = new GameObject();
-        circle.addFixture(cirShape);
-        circle.setMass(MassType.NORMAL);
-        circle.translate(2.0, 2.0);
-        // test adding some force
-        circle.applyForce(new Vector2(-100.0, 0.0));
-        // set some linear damping to simulate rolling friction
-        circle.setLinearDamping(0.05);
-        this.world.addBody(circle);
+        floor1.translate(0.0, -7.0);
+        floor2.translate(0.0, 7.0);
+        this.world.addBody(floor1);
+        this.world.addBody(floor2);
 
-        // try a rectangle
-        Rectangle rectShape = new Rectangle(1.0, 1.0);
-        GameObject rectangle = new GameObject();
-        rectangle.addFixture(rectShape);
-        rectangle.setMass(MassType.NORMAL);
-        rectangle.translate(0.0, 2.0);
-        rectangle.getLinearVelocity().set(-5.0, 0.0);
-        this.world.addBody(rectangle);
+        GameObject wall1 = new GameObject(), wall2 = new GameObject();
 
-        // try a polygon with lots of vertices
-        Polygon polyShape = Geometry.createUnitCirclePolygon(10, 1.0);
-        GameObject polygon = new GameObject();
-        polygon.addFixture(polyShape);
-        polygon.setMass(MassType.NORMAL);
-        polygon.translate(-2.5, 2.0);
-        // set the angular velocity
-        polygon.setAngularVelocity(Math.toRadians(-20.0));
-        this.world.addBody(polygon);
+        BodyFixture fixture = new BodyFixture(new Rectangle(0.2, 14.0));
+        wall1.addFixture(fixture);
+        wall1.setMass(MassType.INFINITE);
+        wall2.addFixture(fixture);
+        wall2.setMass(MassType.INFINITE);
 
-        // try a compound object
-        Circle c1 = new Circle(0.5);
-        BodyFixture c1Fixture = new BodyFixture(c1);
-        c1Fixture.setDensity(0.5);
-        Circle c2 = new Circle(0.5);
-        BodyFixture c2Fixture = new BodyFixture(c2);
-        c2Fixture.setDensity(0.5);
-        Rectangle rm = new Rectangle(2.0, 1.0);
-        // translate the circles in local coordinates
-        c1.translate(-1.0, 0.0);
-        c2.translate(1.0, 0.0);
-        GameObject capsule = new GameObject();
-        capsule.addFixture(c1Fixture);
-        capsule.addFixture(c2Fixture);
-        capsule.addFixture(rm);
-        capsule.setMass(MassType.NORMAL);
-        capsule.translate(0.0, 4.0);
-        this.world.addBody(capsule);
+        wall1.translate(-10.0, 0);
+        wall2.translate(10.0, 0.0);
+        this.world.addBody(wall1);
+        this.world.addBody(wall2);
 
-        GameObject issTri = new GameObject();
-        issTri.addFixture(Geometry.createIsoscelesTriangle(1.0, 3.0));
-        issTri.setMass(MassType.NORMAL);
-        issTri.translate(2.0, 3.0);
-        this.world.addBody(issTri);
 
-        GameObject equTri = new GameObject();
-        equTri.addFixture(Geometry.createEquilateralTriangle(2.0));
-        equTri.setMass(MassType.NORMAL);
-        equTri.translate(3.0, 3.0);
-        this.world.addBody(equTri);
+        // ball
+        GameObject ball = new GameObject();
+        BodyFixture ballFixture = new BodyFixture(new Circle(0.3));
+        ballFixture.setRestitution(1.0);
+        ball.addFixture(ballFixture);
+        ball.setMass(MassType.NORMAL);
+        ball.applyForce(new Vector2(-150.0, 0.0));
+        ball.setLinearDamping(0.05);
+        this.world.addBody(ball);
 
-        GameObject rightTri = new GameObject();
-        rightTri.addFixture(Geometry.createRightTriangle(2.0, 1.0));
-        rightTri.setMass(MassType.NORMAL);
-        rightTri.translate(4.0, 3.0);
-        this.world.addBody(rightTri);
-
-        GameObject cap = new GameObject();
-        cap.addFixture(new Capsule(1.0, 0.5));
-        cap.setMass(MassType.NORMAL);
-        cap.translate(-3.0, 3.0);
-        this.world.addBody(cap);
-
-        GameObject slice = new GameObject();
-        slice.addFixture(new Slice(0.5, Math.toRadians(120)));
-        slice.setMass(MassType.NORMAL);
-        slice.translate(-3.0, 3.0);
-        this.world.addBody(slice);
+        // players
+        GameObject player1 = new GameObject(), player2 = new GameObject();
+        player1.addFixture(new Circle(0.7));
+        player1.setMass(MassType.NORMAL);
+        player1.translate(-9.0, 0.0);
+        player2.addFixture(new Circle(0.7));
+        player2.translate(9.0, 0.0);
+        player2.setMass(MassType.NORMAL);
+        this.world.addBody(player1);
+        this.world.addBody(player2);
     }
 
 
@@ -177,7 +127,7 @@ public class ExampleGraphics2D extends JFrame {
         // before we render everything im going to flip the y axis and move the
         // origin to the center (instead of it being in the top left corner)
         AffineTransform yFlip = AffineTransform.getScaleInstance(1, -1);
-        AffineTransform move = AffineTransform.getTranslateInstance(400, -300);
+        AffineTransform move = AffineTransform.getTranslateInstance(size.getWidth()/2, -size.getHeight()/2);
         g.transform(yFlip);
         g.transform(move);
 
@@ -202,10 +152,7 @@ public class ExampleGraphics2D extends JFrame {
 
     protected void render(Graphics2D canvas) {
         canvas.setColor(Color.WHITE);
-        canvas.fillRect(-400, -300, 800, 600);
-
-        // lets move the view up some
-        canvas.translate(0.0, -1.0 * SCALE);
+        canvas.fillRect((int)(-size.getWidth()/2), (int) (-size.getHeight()/2), (int)size.getWidth(), (int)size.getHeight());
 
         for (int i = 0; i < this.world.getBodyCount(); i++) {
             GameObject go = (GameObject) this.world.getBody(i);
