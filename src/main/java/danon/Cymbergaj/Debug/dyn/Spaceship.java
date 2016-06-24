9d01a2b693c78dfc43493b4e8dc865aa02a1eb0a
Daniel <wilkowski.kontakt@gmail.com>
2016-06-25 00:52:53 +0100

    Replaced Spaceship

--- src/main/java/danon/Cymbergaj/Debug/dyn/Spaceship.java
+++ src/main/java/danon/Cymbergaj/Debug/dyn/Spaceship.java
@@ -2,20 +2,24 @@ package danon.Cymbergaj.Debug.dyn;
 
 import danon.Cymbergaj.Model.World.Control.ControlKeys;
 import danon.Cymbergaj.Model.World.Control.Keys;
+import danon.Cymbergaj.View.Renderer.ImagesRepository;
+import danon.Cymbergaj.View.Renderer.Renderable;
+import danon.Cymbergaj.View.Renderer.SpaceshipRenderer;
 import org.dyn4j.geometry.Vector2;
 
 import java.awt.*;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 
-public class Player extends GameObject implements KeyListener {
+public class Spaceship extends GameObject implements KeyListener {
 
     public final static double VELOCITY = 6.0;
 
+    private double traveledDistance = 0;
     private final ControlKeys controlKeys;
     private final Keys keys;
 
-    public Player(ControlKeys controlKeys) {
+    public Spaceship(ControlKeys controlKeys) {
         this.keys = new Keys();
         this.controlKeys = controlKeys;
         this.controlKeys.useKeys(this.keys);
@@ -24,6 +28,18 @@ public class Player extends GameObject implements KeyListener {
     @Override
     public void renderMe(Graphics2D canvas) {
 
+    }
+
+    public Renderable getRenderer(ImagesRepository images) {
+        return new SpaceshipRenderer(this, images);
+    }
+
+    public double getTraveledDistance() {
+        return traveledDistance;
+    }
+
+    public void incTraveledDistance(double velocity) {
+        this.traveledDistance += velocity;
     }
 
     public void updateMe(double elapsedTime) {
