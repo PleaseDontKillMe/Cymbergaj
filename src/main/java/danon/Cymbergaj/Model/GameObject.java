93fe724b945d31992ca82661fbfb8a42d42c85bb
Daniel <wilkowski.kontakt@gmail.com>
2016-06-25 06:05:38 +0100

    Clean

--- src/main/java/danon/Cymbergaj/Model/GameObject.java
+++ src/main/java/danon/Cymbergaj/Model/GameObject.java
@@ -5,17 +5,7 @@ import danon.Cymbergaj.View.Renderer.ImagesRepository;
 import danon.Cymbergaj.View.Renderer.Renderer;
 import org.dyn4j.dynamics.Body;
 
-import java.awt.*;
-
 abstract public class GameObject extends Body {
-    private Color color;
-
-    public GameObject() {
-        this.color = new Color(
-                (float) Math.random() * 0.5f + 0.5f,
-                (float) Math.random() * 0.5f + 0.5f,
-                (float) Math.random() * 0.5f + 0.5f);
-    }
 
     abstract public Renderer getRenderer(ImagesRepository images);
 }
