ae2f070788ff987a61f25e93e887bc7b342a8d96
Daniel <wilkowski.kontakt@gmail.com>
2016-06-25 07:13:19 +0100

    Displaying points

--- src/main/java/danon/Cymbergaj/View/PointsRenderer.java
+++ src/main/java/danon/Cymbergaj/View/PointsRenderer.java
@@ -15,11 +15,24 @@ public class PointsRenderer implements Renderer {
 
     @Override
     public void renderOn(Graphics2D canvas) {
-
         canvas.setFont(new Font("Arial", 0, 500));
         canvas.setColor(new Color(255, 255, 255, 60));
-        canvas.drawString(game.getPoints1() + "", -360, 160);
-        canvas.drawString(":", -70, 160);
-        canvas.drawString(game.getPoints2() + "", 95, 160);
+        drawPoint(canvas, game.getPoints1(), -360);
+        drawSemicolon(canvas);
+        drawPoint(canvas, game.getPoints2(), 95);
     }
+
+    private void drawSemicolon(Graphics2D canvas) {
+        if (game.getPoints1() > 0 & game.getPoints2() > 0) {
+            canvas.drawString(":", -70, 160);
+        }
+    }
+
+    private void drawPoint(Graphics2D canvas, int points, int x) {
+        if (points > 0) {
+            canvas.drawString(String.valueOf(points), x, 160);
+        }
+    }
+
+
 }
