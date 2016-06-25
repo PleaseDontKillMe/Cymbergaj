ad622cad13450a1a5e6a72e879bce9a44ec39e47
Daniel <wilkowski.kontakt@gmail.com>
2016-06-25 04:54:11 +0100

    Changed WorldRenderer to GameRenderer

--- src/main/java/danon/Cymbergaj/View/Renderer/GameRenderer.java
+++ src/main/java/danon/Cymbergaj/View/Renderer/GameRenderer.java
@@ -1,26 +1,25 @@
 package danon.Cymbergaj.View.Renderer;
 
-import danon.Cymbergaj.Model.World.World;
+import danon.Cymbergaj.Game;
 
 import java.awt.*;
 import java.awt.image.BufferedImage;
 
-public class WorldRenderer implements Renderable {
-    private final World world;
+public class GameRenderer implements Renderer {
+    private final Game game;
     private final BufferedImage background;
 
-    private final static BasicStroke dashed = new BasicStroke(2.0f,
-            BasicStroke.CAP_BUTT, BasicStroke.JOIN_MITER, 10.0f, new float[]{10.0f}, 0.0f);
+    private final static BasicStroke dashed = new BasicStroke(2.0f, BasicStroke.CAP_BUTT, BasicStroke.JOIN_MITER, 10.0f, new float[]{10.0f}, 0.0f);
 
-    public WorldRenderer(World world, ImagesRepository images) {
-        this.world = world;
+    public GameRenderer(Game game, ImagesRepository images) {
+        this.game = game;
         this.background = images.background2;
     }
 
     @Override
     public void renderOn(Graphics2D canvas) {
         int width = background.getWidth();
-        int x = world.backgroundX() % width;
+        int x = game.getBackgroundXSlide() % width;
 
         canvas.drawImage(background, -(x), 0, null);
         canvas.drawImage(background, -(x - width), 0, null);
