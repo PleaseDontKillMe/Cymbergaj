99b36a0a844aaacc47a231afd323fd405b898ba1
Daniel <wilkowski.kontakt@gmail.com>
2016-06-25 01:37:19 +0100

    Refactored WindowCLosingListener

---
+++ src/main/java/danon/Cymbergaj/WindowClosingListener.java
@@ -0,0 +1,31 @@
+package danon.Cymbergaj;
+
+import java.awt.event.WindowEvent;
+import java.awt.event.WindowListener;
+
+public interface WindowClosingListener extends WindowListener {
+
+    @Override
+    default void windowOpened(WindowEvent e) {
+    }
+
+    @Override
+    default void windowClosed(WindowEvent e) {
+    }
+
+    @Override
+    default void windowIconified(WindowEvent e) {
+    }
+
+    @Override
+    default void windowDeiconified(WindowEvent e) {
+    }
+
+    @Override
+    default void windowActivated(WindowEvent e) {
+    }
+
+    @Override
+    default void windowDeactivated(WindowEvent e) {
+    }
+}
