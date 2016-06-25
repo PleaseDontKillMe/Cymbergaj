c7916b963ed40a597af59726f83d49769052c541
Daniel <wilkowski.kontakt@gmail.com>
2016-06-25 04:53:45 +0100

    Implemented ClearScreenRenderer

---
+++ src/main/java/danon/Cymbergaj/View/Renderer/ClearScreenRenderer.java
@@ -0,0 +1,20 @@
+package danon.Cymbergaj.View.Renderer;
+
+
+import java.awt.*;
+
+public class ClearScreenRenderer implements Renderer {
+
+    private Dimension dimension;
+
+    public ClearScreenRenderer(Dimension dimension) {
+        this.dimension = dimension;
+    }
+
+    @Override
+    public void renderOn(Graphics2D canvas) {
+        canvas.setColor(Color.WHITE);
+        canvas.fillRect((int) (-dimension.getWidth() / 2), (int) (-dimension.getHeight() / 2), (int) dimension.getWidth(), (int) dimension.getHeight());
+    }
+
+}
