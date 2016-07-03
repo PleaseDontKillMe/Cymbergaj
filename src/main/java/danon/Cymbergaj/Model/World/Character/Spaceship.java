aca29aab1f27ebe6533a36dcb46252e3044c569e
Daniel <wilkowski.kontakt@gmail.com>
2016-07-03 19:11:58 +0100

    Clean

--- src/main/java/danon/Cymbergaj/Model/World/Character/Spaceship.java
+++ src/main/java/danon/Cymbergaj/Model/World/Character/Spaceship.java
@@ -7,7 +7,7 @@ import danon.Cymbergaj.View.Renderer.ImagesRepository;
 import danon.Cymbergaj.View.Renderer.SpaceshipRenderer;
 import org.dyn4j.dynamics.BodyFixture;
 import org.dyn4j.geometry.Circle;
-import org.dyn4j.geometry.Mass;
+import org.dyn4j.geometry.Mass.Type;
 import org.dyn4j.geometry.Vector2;
 
 import java.awt.event.KeyEvent;
@@ -32,7 +32,7 @@ public class Spaceship extends GameObject implements KeyListener, Updatable {
         BodyFixture fixture = new BodyFixture(new Circle(0.7));
         fixture.setFriction(0.0);
         this.addFixture(fixture);
-        this.setMass(Mass.Type.NORMAL);
+        this.setMass(Type.NORMAL);
     }
 
     public SpaceshipRenderer getRenderer(ImagesRepository images) {
