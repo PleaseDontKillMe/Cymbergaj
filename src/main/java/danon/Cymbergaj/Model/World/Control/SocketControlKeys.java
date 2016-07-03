11e4b54561a01a38d7ef583339cb25156c78e42d
Daniel <wilkowski.kontakt@gmail.com>
2016-07-03 19:16:57 +0100

    Moved socket keys code inside SocketControlKeys

--- src/main/java/danon/Cymbergaj/Model/World/Control/SocketControlKeys.java
+++ src/main/java/danon/Cymbergaj/Model/World/Control/SocketControlKeys.java
@@ -22,4 +22,35 @@ public class SocketControlKeys extends ControlKeys {
     public boolean verifyRight(KeyEvent event) {
         return false;
     }
+
+    public void acceptKeyChange(String response) {
+        switch (response) {
+            case "KEYS UP____0":
+                keys.setUp(false);
+                break;
+            case "KEYS DOWN__0":
+                keys.setDown(false);
+                break;
+            case "KEYS LEFT__0":
+                keys.setLeft(false);
+                break;
+            case "KEYS RIGHT_0":
+                keys.setRight(false);
+                break;
+            case "KEYS UP____1":
+                keys.setUp(true);
+                break;
+            case "KEYS DOWN__1":
+                keys.setDown(true);
+                break;
+            case "KEYS LEFT__1":
+                keys.setLeft(true);
+                break;
+            case "KEYS RIGHT_1":
+                keys.setRight(true);
+                break;
+            default:
+                System.out.println("Inny komunikat: " + response);
+        }
+    }
 }
