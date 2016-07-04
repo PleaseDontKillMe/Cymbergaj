643dd0e52c9bb76cb9787663cb48a07188ef62f3
Daniel <wilkowski.kontakt@gmail.com>
2016-07-04 22:14:11 +0100

    Clean

--- src/main/java/danon/Chat/Chat/ChatClient.java
+++ src/main/java/danon/Chat/Chat/ChatClient.java
@@ -4,11 +4,21 @@ import java.net.*;
 import java.io.*;
 
 public class ChatClient implements Runnable {
-    private Socket socket = null;
-    private Thread thread = null;
-    private DataInputStream console = null;
-    private DataOutputStream streamOut = null;
-    private ChatClientThread client = null;
+    private Socket socket;
+    private Thread thread = new Thread(this);
+    private DataInputStream console;
+    private DataOutputStream streamOut;
+    private ChatClientThread client;
+
+    public static void main(String args[]) throws IOException {
+        if (args.length != 2) {
+            System.out.println("Usage: java ChatClient host port");
+            return;
+        }
+
+        ChatClient client = new ChatClient(args[0], Integer.parseInt(args[1]));
+        client.start();
+    }
 
     private ChatClient(String serverName, int serverPort) {
         System.out.println("Establishing connection. Please wait ...");
@@ -45,18 +55,14 @@ public class ChatClient implements Runnable {
     private void start() throws IOException {
         console = new DataInputStream(System.in);
         streamOut = new DataOutputStream(socket.getOutputStream());
-        if (thread == null) {
-            client = new ChatClientThread(this, socket);
-            thread = new Thread(this);
-            thread.start();
-        }
+
+        client = new ChatClientThread(this, socket);
+        thread.start();
     }
 
     void stop() {
-        if (thread != null) {
-            thread.stop();
-            thread = null;
-        }
+        thread.stop();
+
         try {
             if (console != null) console.close();
             if (streamOut != null) streamOut.close();
@@ -66,15 +72,5 @@ public class ChatClient implements Runnable {
         }
         client.close();
         client.stop();
-    }
-
-    public static void main(String args[]) throws IOException {
-        if (args.length != 2) {
-            System.out.println("Usage: java ChatClient host port");
-            return;
-        }
-
-        ChatClient client = new ChatClient(args[0], Integer.parseInt(args[1]));
-        client.start();
     }
 }
