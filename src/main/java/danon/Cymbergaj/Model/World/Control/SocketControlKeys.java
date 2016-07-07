8a8c3557bb24de4f60546fbbc0746f9583486204
Daniel <wilkowski.kontakt@gmail.com>
2016-07-07 14:07:24 +0200

    SocketControlKeys accepts object instead of string

--- src/main/java/danon/Cymbergaj/Model/World/Control/SocketControlKeys.java
+++ src/main/java/danon/Cymbergaj/Model/World/Control/SocketControlKeys.java
@@ -1,5 +1,7 @@
 package danon.Cymbergaj.Model.World.Control;
 
+import danon.Chat.KeyMessage;
+
 import java.awt.event.KeyEvent;
 
 public class SocketControlKeys extends ControlKeys {
@@ -23,34 +25,10 @@ public class SocketControlKeys extends ControlKeys {
         return false;
     }
 
-    public void acceptKeyChange(String response) {
-        switch (response) {
-            case "KEYS UP____0":
-                keys.setUp(false);
-                break;
-            case "KEYS DOWN__0":
-                keys.setDown(false);
-                break;
-            case "KEYS LEFT__0":
-                keys.setLeft(false);
-                break;
-            case "KEYS RIGHT_0":
-                keys.setRight(false);
-                break;
-            case "KEYS UP____1":
-                keys.setUp(true);
-                break;
-            case "KEYS DOWN__1":
-                keys.setDown(true);
-                break;
-            case "KEYS LEFT__1":
-                keys.setLeft(true);
-                break;
-            case "KEYS RIGHT_1":
-                keys.setRight(true);
-                break;
-            default:
-                System.out.println("Inny komunikat: " + response);
-        }
+    public void acceptKeyChange(KeyMessage message) {
+        keys.setUp(message.isKeyUp());
+        keys.setDown(message.isKeyDown());
+        keys.setLeft(message.isKeyLeft());
+        keys.setRight(message.isKeyRight());
     }
 }
