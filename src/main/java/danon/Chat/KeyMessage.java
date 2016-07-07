5dfa36899f239ce3832e7f42f6208738d87d4b62
Daniel <h4t3r2.g0nn4.ha73@gmail.com>
2016-07-07 23:34:48 +0200

    Added player to KeyMessage

--- src/main/java/danon/Chat/KeyMessage.java
+++ src/main/java/danon/Chat/KeyMessage.java
@@ -3,20 +3,22 @@ package danon.Chat;
 import danon.Cymbergaj.Model.World.Control.Keys;
 
 public class KeyMessage implements Message {
+    private char player;
     private boolean keyUp;
     private boolean keyDown;
     private boolean keyLeft;
     private boolean keyRight;
 
-    private KeyMessage(boolean keyUp, boolean keyDown, boolean keyLeft, boolean keyRight) {
+    private KeyMessage(char player, boolean keyUp, boolean keyDown, boolean keyLeft, boolean keyRight) {
+        this.player = player;
         this.keyUp = keyUp;
         this.keyDown = keyDown;
         this.keyLeft = keyLeft;
         this.keyRight = keyRight;
     }
 
-    public static KeyMessage fromKeys(Keys keys) {
-        return new KeyMessage(keys.isUp(), keys.isDown(), keys.isLeft(), keys.isRight());
+    public static KeyMessage fromKeys(char player, Keys keys) {
+        return new KeyMessage(player, keys.isUp(), keys.isDown(), keys.isLeft(), keys.isRight());
     }
 
     public boolean isKeyUp() {
@@ -33,5 +35,9 @@ public class KeyMessage implements Message {
 
     public boolean isKeyRight() {
         return keyRight;
+    }
+
+    public char getPlayer() {
+        return player;
     }
 }
