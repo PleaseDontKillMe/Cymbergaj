ca260ec64f96bc6f668bee79c534b33ac3422cfd
Daniel <h4t3r2.g0nn4.ha73@gmail.com>
2016-07-10 01:34:20 +0200

    Implemented handgun sprites

--- src/main/java/danon/Cymbergaj/View/Renderer/ImagesRepository.java
+++ src/main/java/danon/Cymbergaj/View/Renderer/ImagesRepository.java
@@ -14,14 +14,22 @@ public class ImagesRepository {
     SpriteSheet fireball;
     SpriteSheet spaceship;
 
-    SpriteSheet handgun;
+    SpriteSheet handgunIdle;
+    SpriteSheet handgunMelee;
+    SpriteSheet handgunMove;
+    SpriteSheet handgunReload;
+    SpriteSheet handgunShoot;
 
     public void load() {
         background = Loader.image("space-background.png");
         fireball = new PackedSpriteSheet(Loader.image("fireball.png"), new Size(64, 64), 8);
         spaceship = new PackedSpriteSheet(Loader.image("spaceship2.png"), new Size(64, 64), 32);
 
-        handgun = new MultifileSpriteSheet("character/handgun/idle/survivor-idle_handgun_", 20).loadImages();
+        handgunIdle = new MultifileSpriteSheet("character/handgun/idle/survivor-idle_handgun_", 20).loadImages();
+        handgunMelee = new MultifileSpriteSheet("character/handgun/meleeattack/survivor-meleeattack_handgun_", 15).loadImages();
+        handgunMove = new MultifileSpriteSheet("character/handgun/move/survivor-move_handgun_", 20).loadImages();
+        handgunReload = new MultifileSpriteSheet("character/handgun/reload/survivor-reload_handgun_", 15).loadImages();
+        handgunShoot = new MultifileSpriteSheet("character/handgun/shoot/survivor-shoot_handgun_", 3).loadImages();
     }
 
 }
