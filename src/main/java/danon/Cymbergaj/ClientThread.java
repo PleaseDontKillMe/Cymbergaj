c35158b0cba62a0bc438bfe4e6af70fdfb41f245
Daniel <wilkowski.kontakt@gmail.com>
2016-07-09 15:53:27 +0100

    Not closing socket on unwanted message

--- src/main/java/danon/Cymbergaj/ClientThread.java
+++ src/main/java/danon/Cymbergaj/ClientThread.java
@@ -43,10 +43,9 @@ class ClientThread extends Thread {
                 client.handle(message);
             } catch (IOException ioe) {
                 System.out.println("Listening error: " + ioe.getMessage());
+                client.finnish();
             } catch (ClassNotFoundException e) {
                 e.printStackTrace();
-            } finally {
-                client.finnish();
             }
         }
     }
