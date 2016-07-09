3afcfe33a41440993eb16579c07561bb1e3fd265
Daniel <h4t3r2.g0nn4.ha73@gmail.com>
2016-07-10 01:26:51 +0200

    Chyba implemented CharacterRenderer

---
+++ src/main/java/danon/Cymbergaj/View/Renderer/CharacterRenderer.java
@@ -0,0 +1,33 @@
+package danon.Cymbergaj.View.Renderer;
+
+import danon.Cymbergaj.Geometry.*;
+import danon.Cymbergaj.Geometry.Point;
+import danon.Cymbergaj.Model.Updatable;
+import danon.Cymbergaj.View.SpriteSheet;
+
+import java.awt.*;
+
+public class CharacterRenderer implements Renderer, Updatable {
+
+    private final SpriteSheet spriteSheet;
+    private int refreshesToNextFrame = 0;
+
+    private double totalElapsedTime = 0;
+
+    public CharacterRenderer(ImagesRepository imagesRepository) {
+        this.spriteSheet = imagesRepository.handgun;
+    }
+
+    @Override
+    public void renderOn(Graphics2D canvas) {
+        Size size = spriteSheet.getSpriteSize();
+        spriteSheet.drawOn(canvas, new Point(-size.getWidth()/2, -size.getHeight()/2));
+    }
+
+    @Override
+    public void update(double elapsedTime) {
+        totalElapsedTime += elapsedTime;
+        int frame = (int) Math.round(totalElapsedTime * 40.0);
+        spriteSheet.setFrame(frame);
+    }
+}
