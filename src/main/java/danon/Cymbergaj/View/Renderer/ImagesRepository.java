f15b1f4e9183911aaea57b8acb73b9f9941c01a4
Daniel <h4t3r2.g0nn4.ha73@gmail.com>
2016-07-10 00:45:27 +0200

    Extracted image loader

--- src/main/java/danon/Cymbergaj/View/Renderer/ImagesRepository.java
+++ src/main/java/danon/Cymbergaj/View/Renderer/ImagesRepository.java
@@ -2,13 +2,11 @@ package danon.Cymbergaj.View.Renderer;
 
 
 import danon.Cymbergaj.Geometry.Size;
+import danon.Cymbergaj.View.Loader;
 import danon.Cymbergaj.View.PackedSpriteSheet;
 import danon.Cymbergaj.View.SpriteSheet;
 
-import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage;
-import java.io.IOException;
-import java.net.URL;
 
 public class ImagesRepository {
     BufferedImage background;
@@ -16,22 +14,9 @@ public class ImagesRepository {
     SpriteSheet spaceship;
 
     public void load() {
-        background = loadImage("space-background.png");
-        fireball = new PackedSpriteSheet(loadImage("fireball.png"), new Size(64, 64), 8);
-        spaceship = new PackedSpriteSheet(loadImage("spaceship2.png"), new Size(64, 64), 32);
+        background = Loader.image("space-background.png");
+        fireball = new PackedSpriteSheet(Loader.image("fireball.png"), new Size(64, 64), 8);
+        spaceship = new PackedSpriteSheet(Loader.image("spaceship2.png"), new Size(64, 64), 32);
     }
 
-    private BufferedImage loadImage(String filename) {
-        URL resource = getClass().getClassLoader().getResource(filename);
-
-        if (resource == null) {
-            throw new RuntimeException("File " + filename + " not found");
-        }
-
-        try {
-            return ImageIO.read(resource);
-        } catch (IOException ignored) {
-            throw new RuntimeException("File " + filename + " not found");
-        }
-    }
 }
