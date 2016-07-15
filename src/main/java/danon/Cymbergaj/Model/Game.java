11c23810ac916e72e5c01dd76b8745c1def45ff8
Daniel <wilkowski.kontakt@gmail.com>
2016-07-15 22:17:59 +0100

    Clean

--- src/main/java/danon/Cymbergaj/Model/Game.java
+++ src/main/java/danon/Cymbergaj/Model/Game.java
@@ -7,7 +7,6 @@ import danon.Cymbergaj.View.Renderer.ImagesRepository;
 import danon.Cymbergaj.View.Renderer.Renderer;
 
 public class Game implements Updatable {
-
     private int pointsLeft = 0, pointsRight = 0;
     private PreviousWinner previousWinner = PreviousWinner.None;
     private double backgroundXSlide = 0;
@@ -33,8 +32,7 @@ public class Game implements Updatable {
     public void restartGame() {
         if (pointsLeft == pointsRight) {
             previousWinner = PreviousWinner.Tie;
-        }
-        else {
+        } else {
             if (pointsLeft > pointsRight) {
                 previousWinner = PreviousWinner.WsadPlayer;
             } else {
@@ -48,7 +46,7 @@ public class Game implements Updatable {
 
     @Override
     public void update(double elapsedTime) {
-        backgroundXSlide += elapsedTime*50.0;
+        backgroundXSlide += elapsedTime * 50.0;
     }
 
     public int getBackgroundXSlide() {
