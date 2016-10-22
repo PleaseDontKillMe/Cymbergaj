91642c62e9530c87a80875105ea5e0b859f7d47f
Daniel <wilkowski.kontakt@gmail.com>
2016-10-22 13:50:58 +0200

    Added Posture visitor

--- src/main/java/danon/Cymbergaj/Posture.java
+++ src/main/java/danon/Cymbergaj/Posture.java
@@ -1,5 +1,35 @@
 package danon.Cymbergaj;
 
+import danon.Cymbergaj.View.Renderer.Character.CharacterWeaponSheet;
+import danon.Cymbergaj.View.SpriteSheet;
+
 public enum Posture {
-    Idle, Melee, Move, Reload, Shoot
+    Idle {
+        @Override
+        SpriteSheet getSheetFor(CharacterWeaponSheet sheet) {
+            return sheet.getIdle();
+        }
+    }, Melee {
+        @Override
+        SpriteSheet getSheetFor(CharacterWeaponSheet sheet) {
+            return sheet.getMelee();
+        }
+    }, Move {
+        @Override
+        SpriteSheet getSheetFor(CharacterWeaponSheet sheet) {
+            return sheet.getMove();
+        }
+    }, Reload {
+        @Override
+        SpriteSheet getSheetFor(CharacterWeaponSheet sheet) {
+            return sheet.getReload();
+        }
+    }, Shoot {
+        @Override
+        SpriteSheet getSheetFor(CharacterWeaponSheet sheet) {
+            return sheet.getShoot();
+        }
+    };
+
+    abstract SpriteSheet getSheetFor(CharacterWeaponSheet sheet);
 }
