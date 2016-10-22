add70ad42312ccdcf35fbca1567d9514986d78be
Daniel <wilkowski.kontakt@gmail.com>
2016-10-22 16:33:19 +0100

    Moving again

--- src/main/java/danon/Cymbergaj/Character.java
+++ src/main/java/danon/Cymbergaj/Character.java
@@ -91,7 +91,7 @@ public class Character extends GameObject implements KeyListener, Updatable {
         Vector2 velocity = new Vector2();
         velocity.x = Math.cos(orientation) * speed;
         velocity.y = Math.sin(orientation) * speed;
-        // this.setLinearVelocity(velocity);
+        this.setLinearVelocity(velocity);
     }
 
     private void changePosture(Posture newPosture) {
