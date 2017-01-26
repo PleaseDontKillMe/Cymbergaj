7f99e74919e72545fd92f205c9b7f1d22eb34b47
Daniel <wilkowski.kontakt@gmail.com>
2017-01-26 17:29:11 +0100

    Cltr + Alt + L

--- src/main/java/danon/Cymbergaj/View/Renderer/Character/CharacterWeaponSheet.java
+++ src/main/java/danon/Cymbergaj/View/Renderer/Character/CharacterWeaponSheet.java
@@ -4,8 +4,12 @@ import danon.Cymbergaj.View.SpriteSheet;
 
 public interface CharacterWeaponSheet {
     SpriteSheet getIdle();
+
     SpriteSheet getMelee();
+
     SpriteSheet getMove();
+
     SpriteSheet getReload();
+
     SpriteSheet getShoot();
 }
