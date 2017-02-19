3044afede6e7b9ce07864b5811534927aa84b83d
Daniel <wilkowski.kontakt@gmail.com>
2017-02-19 15:47:13 +0100

    Implemented FileLogger

--- src/main/java/danon/Config/log/FileLogger.java
+++ src/main/java/danon/Config/log/FileLogger.java
@@ -1,8 +1,33 @@
 package danon.Config.log;
 
+import java.io.FileWriter;
+import java.io.IOException;
+
 public class FileLogger extends Logger {
+    private FileWriter writer;
+
     @Override
     public void logString(String context) {
+        initializeIfMissing();
+        logMessage(context);
+    }
 
+    private void logMessage(String context) {
+        try {
+            writer.write(context);
+            writer.flush();
+        } catch (IOException e) {
+            throw new RuntimeException("Could not log message", e);
+        }
+    }
+
+    private void initializeIfMissing() {
+        if (writer == null) {
+            try {
+                writer = new FileWriter("debug.log", true);
+            } catch (IOException e) {
+                throw new RuntimeException("Couldn't open log file", e);
+            }
+        }
     }
 }
